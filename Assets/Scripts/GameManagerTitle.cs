80678934cc869f46d291eb0beac41482b8ac1fc6
HirokiYamamoto <hiroki200328@icloud.com>
2022-10-18 16:12:23 +0900

    シェーダーいじいじしたいけどようわからん

--- Assets/Scripts/GameManagerTitle.cs
+++ Assets/Scripts/GameManagerTitle.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class GameManagerTitle : MonoBehaviour
 {
-
+	ControllerCheck controllerCheck;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		Screen.SetResolution(1920, 1080, true);
 		Application.targetFrameRate = 60;
+
+		controllerCheck = GetComponent<ControllerCheck>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		if(controllerCheck)
+		{
+			if(Input.GetButtonDown("buttonX"))
+			{
+				//シーン切り替え
+
+			}
+		}
+		else
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				//シーン切り替え
+
+			}
+		}
 	}
 }
---
+++ Assets/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Material>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
