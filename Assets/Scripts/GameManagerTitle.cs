adacafa24482b1150809f15d2d60928d722c1701
HirokiYamamoto <hiroki200328@icloud.com>
2022-10-25 15:44:29 +0900

    シーン切り替え導入

--- Assets/Scripts/GameManagerTitle.cs
+++ Assets/Scripts/GameManagerTitle.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GameManagerTitle : MonoBehaviour
 {
 	ControllerCheck controllerCheck;
-
+	SceneController sceneController;
+	
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -13,22 +14,26 @@ public class GameManagerTitle : MonoBehaviour
 		Application.targetFrameRate = 60;
 
 		controllerCheck = GetComponent<ControllerCheck>();
+
+		GameObject camera = GameObject.Find("Main Camera");
+		sceneController = camera.GetComponent<SceneController>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+
 		if(controllerCheck)
 		{
-			if(Input.GetButtonDown("buttonX"))
+			if (Input.GetKeyDown(KeyCode.Space))
 			{
 				//シーン切り替え
-
+				sceneController.sceneChange("StageSelect");
 			}
 		}
 		else
 		{
-			if(Input.GetKeyDown(KeyCode.Space))
+			if (Input.GetButtonDown("buttonX"))
 			{
 				//シーン切り替え
 
